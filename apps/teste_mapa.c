13f4d24e26e649e3e838b7c5a16bf944a7911d03
joao-tinelli <joao_tinelli@outlook.com>
2024-05-22 13:25:05 -0300

    criacao do mapa

---
+++ apps/teste_mapa.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "alianca.h"
+#include "edificio.h"
+#include "faccao.h"
+#include "mapa.h"
+#include "mensagens.h"
+#include "unidade.h"
+
+int main(int argc, char const *argv[])
+{
+    Dimensao *d = mapa_le_dimensao("./input/config.txt");
+    int **mapa = mapa_aloca(d);
+    mapa_gera(mapa, d);
+    mapa_display(mapa, d);
+
+    desaloca_mapa(mapa, d);
+     
+    mapa_display(mapa, d);
+
+    return 0;
+}
