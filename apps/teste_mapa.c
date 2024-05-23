88a42aad28027ebf7dfa851093b77c686e46aedd
joao-tinelli <joao_tinelli@outlook.com>
2024-05-23 11:20:41 -0300

    mapa do tipo char com sucesso

--- apps/teste_mapa.c
+++ apps/teste_mapa.c
@@ -9,9 +9,8 @@
 int main(int argc, char const *argv[])
 {
     Dimensao *d = mapa_le_dimensao("./input/config.txt");
-    int **mapa = mapa_aloca(d);
+    char **mapa = mapa_aloca(d);
     mapa_gera(mapa, d);
-    mapa_display(mapa, d);
 
     desaloca_mapa(&mapa, d);
      
