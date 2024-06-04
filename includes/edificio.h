a2a81a0cf2829f62408297ae8dace35d873d0edc
leobglp <borgesleo780@gmail.com>
2024-06-04 14:03:39 -0300

    Adicionado comentários nas funções dentro de include

--- includes/edificio.h
+++ includes/edificio.h
@@ -3,17 +3,64 @@
 
 #include "mapa.h"
 
+/**
+ * @brief Nó da lista;
+ * @typedef TEdificio;
+*/
 typedef struct edificio TEdificio;
+/**
+ * @brief Cabeça da lista;
+ * @typedef CEdificio;
+*/
 typedef struct _cedificio CEdificio;
 
+/**
+ * @brief Aloca uma struct do tipo TEdificio;
+ * @param chave, tipo, qtd, x, y;
+ * @return TEdificio;
+*/
 TEdificio *tedificio_aloca(const char chave, const int tipo, const int qtd, const int x, const int y);
+/**
+ * @brief Aloca uma struct do tipo CFaccao;
+ * @param nome, x, y;
+ * @return TFaccao;
+*/
 CEdificio *cedificio_cria(void);
+/**
+ * @brief Desaloca todo os edificios;
+ * @param cabeca;
+ * @return void;
+*/
 void cedificio_desaloca(CEdificio **cabeca);
-
+/**
+ * @brief Verifica se a lista está vazia;
+ * @param cabeca;
+ * @return (0 ou 1);
+*/
 int cedificio_vazio(const CEdificio *cabeca);
+/**
+ * @brief Insere um edificio na lista;
+ * @param cabeca, novo;
+ * @return void;
+*/
 void edificio_insere(CEdificio *cabeca, TEdificio *novo);
+/**
+ * @brief Imprime a lista dos edifícios;
+ * @param cabeca;
+ * @return void;
+*/
 void edificio_display(const CEdificio *cabeca);
+/**
+ * @brief Utilizada quando um novo edificio for construído;
+ * @param cabeca, identificador, qtd, tipo, x, y;
+ * @return void;
+*/
 void edificio_constroi(CEdificio *cabeca, char *identificador, int qtd, int tipo, int x, int y);
+/**
+ * @brief Junta duas listas do tipo CEdificio;
+ * @param cabeca_1, cabeca_2;
+ * @return void;
+*/
 void edificio_merge(CEdificio *cabeca_1, CEdificio *cabeca_2);
 
 #endif
--- includes/mensagens.h
+++ includes/mensagens.h
@@ -1,7 +1,18 @@
 #ifndef MENSAGENS_H
 #define MENSAGENS_H
 
+/**
+ * @brief Mensagem de erro;
+ * @param S, funcao;
+ * @return void;
+*/
 void msg_erro(char *S, char *funcao);
+
+/**
+ * @brief Mensagem de sucesso;
+ * @param S, funcao;
+ * @return void;
+*/
 void msg_sucesso(char *S, char *funcao);
 
 #endif 
